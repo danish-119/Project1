f7d7abc7387b6efb2960c776dc0c55d12e862d39
Muhammad Danish <mdanishateeq@gmail.com>
2025-07-12 14:24:03 +0500

    _

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
    env: {
     POCKETBASE_URL: process.env.POCKETBASE_URL,
   },
+   eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
